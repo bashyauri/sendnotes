48febc31b3cd01783f1d225a47942c6916e6e61c
Bashar Umar <basharu83@gmail.com>
2024-07-28 17:18:18 +0100

    add policy

--- resources/views/livewire/notes/show-notes.blade.php
+++ resources/views/livewire/notes/show-notes.blade.php
@@ -1,10 +1,17 @@
 <?php
 
 use Livewire\Volt\Component;
-
+use App\Models\Note;
 
 new class extends Component {
 
+    public function delete($noteId){
+        {
+            $note = Note::where('id',$noteId)->first();
+            $this->authorize('delete',$note);
+            $note->delete();
+        }
+    }
     public function with():array
     {
         return [
@@ -25,19 +32,24 @@ new class extends Component {
         @else
              <x-button  primary icon-right="plus" class="mb-12" href="{{route('notes.create')}}" wire:navigate>
                 Create Notes</x-button>
-        <div class="grid grid-cols-2 gap-4">
+        <div class="grid grid-cols-3 gap-4">
         @foreach ($notes as $note)
         <x-card wire:key='{{$note->id}}'>
 
             <div class="flex justify-between">
-                <a href="" class="text-xl font-bold hover:underline hover:text-blue-500">{{$note->title}}</a>
+                <div>
+                <a href="#" class="text-xl font-bold hover:underline hover:text-blue-500">{{$note->title}}</a>
+
+                    <p class="mt-2 text-xs">{{Str::limit($note->body,50)}}</p>
+                </div>
+
                 <div class="text-xs text-gray-500">{{ \Carbon\Carbon::parse($note->send_date)->format('M/d/Y')}}</div>
             </div>
             <div class="flex items-end justify-between mt-4 space-x-1">
                 <p class="text-xs">Recipient: <span class="font-semibold">{{$note->recipient}}</span></p>
                 <div>
-                    <x-mini-button rounded flat gray icon="eye"></x-mini-button>
-                    <x-mini-button rounded flat gray icon="trash"></x-mini-button>
+                    <x-mini-button rounded flat gray icon="eye" ></x-mini-button>
+                    <x-mini-button rounded flat gray icon="trash" wire:click="delete('{{$note->id}}')"></x-mini-button>
 
                 </div>
 
