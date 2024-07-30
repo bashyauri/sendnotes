71548d5aacec56b5a3e36b35c1714922876096fc
Bashar Umar <basharu83@gmail.com>
2024-07-30 07:03:05 +0100

    completed factory

---
+++ database/factories/NoteFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Note>
+ */
+class NoteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => $this->faker->uuid,
+            'user_id' => User::factory(),
+            'title' => $this->faker->sentence,
+            'body' => $this->faker->paragraph,
+            'send_date' => $this->faker->dateTimeBetween('now', '+10 days'),
+            'is_published' => true,
+            'heart_count' => $this->faker->numberBetween(0, 100),
+        ];
+    }
+}
