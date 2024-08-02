f5c84d25728f437551aac94e11c9fea71b82c928
Bashar Umar <basharu83@gmail.com>
2024-08-02 17:59:04 +0100

    create dashboard stats

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,12 +7,11 @@
 
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
+
                 <div class="p-6 text-gray-900">
-                     <x-button primary>Hi there</x-button>
+                    <livewire:dashboardstats />
                 </div>
 
-            </div>
         </div>
     </div>
 </x-app-layout>
